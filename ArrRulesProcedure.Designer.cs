755d25dc82b6b1a773ca35c434009e768005bbe4
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:46:36 +0300

    Add files via upload

---
+++ ArrRulesProcedure.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace _23_variant_CSarp_4_3_3
+{
+    partial class ArrRulesProcedure
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMatrDim = new System.Windows.Forms.Button();
+            this.labelInputEl = new System.Windows.Forms.Label();
+            this.textBoxInputEl = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonMatrDim
+            // 
+            this.buttonMatrDim.Location = new System.Drawing.Point(355, 20);
+            this.buttonMatrDim.Name = "buttonMatrDim";
+            this.buttonMatrDim.Size = new System.Drawing.Size(103, 26);
+            this.buttonMatrDim.TabIndex = 26;
+            this.buttonMatrDim.Text = "Применить";
+            this.buttonMatrDim.UseVisualStyleBackColor = true;
+            this.buttonMatrDim.Click += new System.EventHandler(this.buttonMatrDim_Click);
+            // 
+            // labelInputEl
+            // 
+            this.labelInputEl.AutoSize = true;
+            this.labelInputEl.Font = new System.Drawing.Font("Segoe UI", 9.163636F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelInputEl.Location = new System.Drawing.Point(12, 20);
+            this.labelInputEl.Name = "labelInputEl";
+            this.labelInputEl.Size = new System.Drawing.Size(256, 19);
+            this.labelInputEl.TabIndex = 25;
+            this.labelInputEl.Text = "Введите размерность матриц (N):";
+            // 
+            // textBoxInputEl
+            // 
+            this.textBoxInputEl.Location = new System.Drawing.Point(274, 20);
+            this.textBoxInputEl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxInputEl.Name = "textBoxInputEl";
+            this.textBoxInputEl.Size = new System.Drawing.Size(75, 26);
+            this.textBoxInputEl.TabIndex = 24;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 81);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(903, 132);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 221);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 47;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(903, 132);
+            this.dataGridView2.TabIndex = 28;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(12, 361);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersWidth = 47;
+            this.dataGridView3.RowTemplate.Height = 28;
+            this.dataGridView3.Size = new System.Drawing.Size(917, 132);
+            this.dataGridView3.TabIndex = 29;
+            // 
+            // ArrRulesProcedure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 609);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonMatrDim);
+            this.Controls.Add(this.labelInputEl);
+            this.Controls.Add(this.textBoxInputEl);
+            this.Name = "ArrRulesProcedure";
+            this.Text = "Разработка программы с выделением процедур";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMatrDim;
+        private System.Windows.Forms.Label labelInputEl;
+        private System.Windows.Forms.TextBox textBoxInputEl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
---
+++ ArrRulesProcedure.cs
@@ -0,0 +1,77 @@
+﻿/*Условие: Разработать программу, выделив процедуры.*/
+/**Задача 4.3.3 Условие: даны два целочисленных массива с положительными элементами X1, X2...Xn и Y1, Y2...Yn. 
+Если все элементы массива X меньше всех элементов массива Y с соответствующими индексами, сформировать массив Z1 Z2...Zn
+по правилу */
+
+/*Condition: Develop a program by selecting procedures.*/
+/**Problem 4.3.3 Condition: two integer arrays with positive elements X1, X2...Xn and Y1, Y2...Yn are given.
+If all elements of array X are less than all elements of array Y with corresponding indexes, form an array Z1 Z2...Zn
+according to the rule */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSarp_4_3_3
+{
+    public partial class ArrRulesProcedure : Form
+    {
+        public ArrRulesProcedure()
+        {
+            InitializeComponent();
+        }
+        private void buttonMatrDim_Click(object sender, EventArgs e)
+        {
+            ushort n = ushort.Parse(textBoxInputEl.Text);                           //n x n матрица
+            dataGridView1.RowCount = 1;                                       //кол-во строк
+            dataGridView1.ColumnCount = n;                                    //столбцов    
+            dataGridView2.RowCount = 1;
+            dataGridView2.ColumnCount = n;
+            dataGridView3.RowCount = 1;                                       //одномерный массив С
+            dataGridView3.ColumnCount = n;
+            dataGridView1.Rows[0].HeaderCell.Value = "Матрица X";
+            dataGridView2.Rows[0].HeaderCell.Value = "Матрица Y";
+            dataGridView3.Rows[0].HeaderCell.Value = "Матрица Z";
+            Random rnd = new Random();
+            double[] tableArr_X = new double[n];
+            double[] tableArr_Y = new double[n];
+            double[] tableArr_Z = new double[n];
+            ushort ArrayElLess = 0;                                         //счетчик кол-во элементов меньше
+
+            for (int count_column = 0; count_column < n; count_column++)                                         //заполняем значения X
+            {
+                tableArr_X[count_column] = rnd.Next(0, 25);                 //заполняем массив случайными значениями
+                tableArr_Y[count_column] = rnd.Next(0, 25);
+                dataGridView1.Rows[0].Cells[count_column].Value = tableArr_X[count_column];
+                dataGridView2.Rows[0].Cells[count_column].Value = tableArr_Y[count_column];
+                if (tableArr_X[count_column] < tableArr_Y[count_column])                     //Если элемент Aii меньше хоть одного текущего элемента i-ой строки матрицы
+                    ArrayElLess++;
+            }
+            if (ArrayElLess == n)                                                                     //если все элементы массива X меньше всех эл. массива Y
+            {
+                for (int count_column = 0; count_column < n; count_column++)                                         //заполняем значения X
+                {
+                    tableArr_Z[count_column] = Factorial(tableArr_Y[count_column]) / (Factorial(tableArr_Y[count_column]) * (Factorial(tableArr_Y[count_column]) - Factorial(tableArr_X[count_column])));
+                    dataGridView3.Rows[0].Cells[count_column].Value = tableArr_Z[count_column].ToString("0.######");
+                }
+            }
+            double Factorial(double x)
+            {
+                if (x == 0 || x == 1)                                                           // базовое или частное решение
+                {
+                    return 1;
+                }
+                else
+                {
+                    return x * Factorial(x - 1);                                                //Рекурсивный вызов
+                }
+            }
+        }
+    }
+}
